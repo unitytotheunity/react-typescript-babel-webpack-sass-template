0cdee08fed3e8f32c6727a3d5c460b877c6abb7b
Dillon <dillon.notz@gmail.com>
2020-04-21 21:36:19 -0400

    Add basic Webpack, Typescript, Yarn, and Git configs;

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// `CheckerPlugin` is optional. Use it if you want async error reporting.
+// We need this plugin to detect a `--watch` mode. It may be removed later
+// after https://github.com/webpack/webpack/issues/3460 will be resolved.
+var path = require('path');
+const { CheckerPlugin } = require('awesome-typescript-loader')
+ 
+module.exports = {
+  mode: "production", 
+  // Currently we need to add '.ts' to the resolve.extensions array.
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      src: path.resolve(__dirname, 'src/')
+    }
+  },
+ 
+  // Source maps support ('inline-source-map' also works)
+  devtool: 'source-map',
+ 
+  // Add the loader for .ts files.
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins: [
+      new CheckerPlugin()
+  ],
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM"
+  }
+};
