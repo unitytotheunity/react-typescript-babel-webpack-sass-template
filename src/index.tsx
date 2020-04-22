4f0291c461c2f2aaefc30eeb134eb2118f61ed92
Dillon <dillon.notz@gmail.com>
2020-04-21 23:24:50 -0400

    Add first Sass file for global styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import './scss/global.scss'; // Global styling information
 import { Hello } from "./components/App";
 
 ReactDOM.render(
