29c7fa3d16f1b2cee7528d36ffd3df44e36147a6
Dillon <dillon.notz@gmail.com>
2020-04-23 20:22:05 -0400

    Add HotModuleReplacement, webpack-dev-server, and output modifications to Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,60 +4,75 @@
 var path = require('path');
 const { CheckerPlugin } = require('awesome-typescript-loader')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 console.info(`NODE_ENV: ${process.env.NODE_ENV}`)
  
 module.exports = {
-  mode: isDevelopment ? "development" : "production", 
-  // Currently we need to add '.ts' to the resolve.extensions array.
+  mode: isDevelopment ? "development" : "production",
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss'],
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".scss"],
     alias: {
-      src: path.resolve(__dirname, 'src/')
+      src: path.resolve(__dirname, "src/")
     }
-  }, 
+  },
+
   // Source maps support ('inline-source-map' also works)
-  devtool: 'source-map',
- 
-  // Add the loader for .ts files.
+  devtool: "source-map",
+
+  devtool: isDevelopment ? "source-map" : false,
+  devServer: {
+    hot: true,
+    // compress: true,
+    port: 8080,
+    publicPath: "/dist",
+    liveReload: false
+  },
+
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: [
-          'awesome-typescript-loader'
-        ]
+        loader: ["awesome-typescript-loader"]
       },
       {
         test: /\.module\.s(a|c)ss$/,
         loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          // TODO: Figure out if this works
+          // isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true,
               sourceMap: isDevelopment
             }
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: isDevelopment
             }
-          },
-          'awesome-typescript-loader'
+          }
         ]
       },
       {
         test: /\.s(a|c)ss$/,
         exclude: /\.module.(s(a|c)ss)$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
+        use: [
+          // TODO: Figure out if this works
+          // isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "style-loader",
           {
-            loader: 'sass-loader',
+            loader: "css-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: "sass-loader",
             options: {
               sourceMap: isDevelopment
             }
@@ -67,22 +82,21 @@ module.exports = {
     ]
   },
   plugins: [
-      new CheckerPlugin(),
-      new MiniCssExtractPlugin({
-        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-      })
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+    new CheckerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
+    })
   ],
   externals: {
-    react: "React",
-    "react-dom": "ReactDOM"
-  }
+    react: "React"
+  },
+  output: {
+    filename: isDevelopment ? "main.js" : "bundle.js",
+    path: path.join(__dirname, "dist")
+  },
+  entry: [path.join(__dirname, "src/index.tsx")]
 };
-
-if (!isDevelopment) {
-  module.exports.output = {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-  module.exports.entry = './dist/main.js'
-}
